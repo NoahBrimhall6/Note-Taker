2aa798861c4320aee1196a7495990a346243338a
Noah Brimhall <noahbrimhall6@gmail.com>
2022-10-22 19:12:15 -0600

    restructured the fs system in server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
+const uuid = require('./helpers/uuid.js');
+
 
 const app = express();
-const readFromFile = util.promisify(fs.readFile);
 
 let PORT = process.env.PORT;
 if (PORT == null || PORT == "") {
@@ -16,20 +16,6 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-// read file logic
-var notes;
-const updateNotes = () => {
-  readFromFile('./db/db.json', 'utf8', (err, data) => {
-    if(err) {
-      console.log("Error, Unable to read db.json: " + err);
-    } else {
-      console.log("Success! db.json has been read!");
-    }
-    notes = JSON.parse(data);
-  });
-  return notes;
-};
-
 // routes - what happens when some client sends a GET for '/'
 app.get('/', (req, res) => 
   res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -39,32 +25,55 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-  res.json(updateNotes())
-);
-
-app.post('/api/notes', (req, res) => {
-  var parsedNotes = updateNotes();
-  var { title, text } = req.body;
-  
-  var newNote = {
-    "title": title,
-    "text": text,
-    "id": parsedNotes.length + 1
-  };
-
-  parsedNotes.push(newNote);
-
-  // Write updated reviews back to the file
-  fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) => {
-    if(writeErr) {
-      console.error('Unable to update db.json' + writeErr);
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
     } else {
-      console.info('Successfully updated notes!');
+      res.json(JSON.parse(data));
     }
   });
+});
 
-  res.status(200).json(newNote);
+app.post('/api/notes', (req, res) => {
+
+  var { title, text } = req.body;
+  
+  if (title && text) {
+    var newNote = {
+      "title": title,
+      "text": text,
+      "id": uuid()
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) => {
+          if(writeErr) {
+            console.error('Unable to update db.json' + writeErr);
+          } else {
+            console.info('Successfully updated notes!');
+          }
+        });
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
 });
 
 // set up listener
